ec392751e5a2e3218cb2602eff29439a2a897ee6
fofvvk-png <fofvvk@gmail.com>
2026-08-18 21:46:15 +0400

    Implement startGameWithPlayer function
    Add function to start game and set up screen orientation

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+function startGameWithPlayer() {
+  // ... (əvvəlki kodların)
+
+  // Ekranı avtomatik üfüqi rejimə çevirib kilidləyir
+  if (screen.orientation && screen.orientation.lock) {
+    screen.orientation.lock('landscape').catch((err) => {
+      console.log("Avtomatik kilidləmə dəstəklənmədi, istifadəçi özü çevirməlidir:", err);
+    });
+  }
+
+  // Canvas ölçüsünü JS daxilində yeniləyirik
+  canvas.width = 500;
+  canvas.height = 320;
+  
+  // Player-in y oxunu yeni boyuta uyğunlaşdırırıq
+  player.y = 230; 
+
+  gameLoop();
+}
